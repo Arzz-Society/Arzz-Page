56dd860c341a8623e055bf7f01152544c0238931
Michael_Arzz <michaelrzjane@gmail.com>
2025-07-30 21:02:30 -0400

    Actualizacion

--- Store/script.js
+++ Store/script.js
@@ -1,20 +1,2 @@
 document.addEventListener("DOMContentLoaded", () => {
-  lucide.createIcons();
 });
-
-function navigateToPage(url) {
-  const body = document.body;
-  body.classList.add('fade-out');
-
-  setTimeout(() => {
-    window.location.href = url;
-  }, 500);
-}
-
-const btnIncio = document.getElementById("btn-inicio");
-if (btnInicio) {
-  console.log('no mames we');
-  btnInicio.addEventListener('click', () => {
-    navigateToPage('../Inicio/index.html');
-  });
-}
